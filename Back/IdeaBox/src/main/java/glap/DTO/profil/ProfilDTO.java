7eadf218f17dd1c936d684589a06b4f46f7970ae
Alban <Alban.fooz@gmail.com>
2019-11-18 15:17:41 +0100

    profil DTO, Repository & Iservice

---
+++ Back/IdeaBox/src/main/java/glap/DTO/profil/ProfilDTO.java
@@ -0,0 +1,18 @@
+package glap.DTO.profil;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfilDTO {
+	Integer id;
+	String login; //Pseudo
+	String mdp;
+	String nom;
+	String prenom;
+	LocalDateTime createdAt;
+}
---
+++ Back/IdeaBox/src/main/java/glap/repository/IProfilRepository.java
@@ -0,0 +1,9 @@
+package glap.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import glap.model.Profil;
+
+public interface IProfilRepository extends PagingAndSortingRepository<Profil, Integer> {
+
+}
