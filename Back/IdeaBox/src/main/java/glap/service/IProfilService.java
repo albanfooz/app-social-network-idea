fe4f8d95a8385fa4527af58998228b65289778e9
Alban <Alban.fooz@gmail.com>
2019-11-19 09:09:18 +0100

    oups, oublie de sauvegarde

--- Back/IdeaBox/src/main/java/glap/service/IProfilService.java
+++ Back/IdeaBox/src/main/java/glap/service/IProfilService.java
@@ -3,11 +3,36 @@
  */
 package glap.service;
 
+import java.util.List;
+
+import glap.DTO.profil.ProfilDTO;
+
 /**
  *
  * @author Alban
  *
  */
 public interface IProfilService {
+	/**
+	 * Modifier le Profil
+	 *
+	 * @param id        du profil à modifier dans la base
+	 * @param profilDTO à remplacer
+	 * @return profilDTO si OK, null sinon
+	 */
+	public ProfilDTO update(Integer id, ProfilDTO profilDTO);
+	/**
+	 * Ajouter le Profil
+	 *
+	 * @param profilDTO à ajouter
+	 * @return profilDTO si OK, null sinon
+	 */
+	public ProfilDTO add(ProfilDTO profilDTO) ;
+	/**
+	 * Afficher les Profils
+	 *
+	 * @return List<profilDTO> si OK, null sinon
+	 */
+	public List<ProfilDTO> recupererAll();
 
 }
